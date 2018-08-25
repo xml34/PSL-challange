693c10fe0792a43926424e00459f9cbb24cfe1e8
xml34 <eafitense@gmail.com>
2018-08-25 12:58:11 -0500

    80% realizado falta control de errores, implementar ganar, autmatizar pruebas implementarle un mvc

--- Board.java
+++ Board.java
@@ -1,118 +1,171 @@
 import java.util.Random;  
-public class Board{
+public class Board{    
+    private String[][] board1,board2;
+    private int[][] board3;
+    private int rows,cols,mins,contRight,contFlags;
+    private Random aleatorio = new Random(System.currentTimeMillis());
     
-	private String[][] board1,board2;
-	private int rows,cols,mins;
-	private int[][] board3;
-	private Random aleatorio = new Random(System.currentTimeMillis());
-	
-	public Board(){}
+    public Board(){}
 
-	public void newGame(String[] in){
-	    rows=parse(in[0]);
-	    cols=parse(in[1]);
-	    mins=parse(in[2]);
-		board1 = new String[rows][cols];
-		board2 = new String[rows][cols];
-		board3 = new int[rows][cols];
-		for(int i =0; i < rows;i++){
-		    for(int j =0; j < cols;j++){
-		        board1[i][j]=" . ";
-		        board2[i][j]=" . ";
-		    }
-		}
-		setMines();
-		printBoard();
-		printBoard2();
-	}
-
-	public static int parse(String x){
-		return Integer.parseInt(x);
-	}
-	
-	public void setMines(){
-	      int cont=0,i,j;
-	      while(cont <mins){
-	          i=aleatorio.nextInt(rows);
-	          j=aleatorio.nextInt(cols);
-	          if(board2[i][j].equals(" . ")){
-	              board2[i][j]=" * ";
-	              cont ++;
-	          }	          
-	      }
-	}
-
-	public void mark(String[] in){
-	    int row=parse(in[0]);
-	    int col=parse(in[1]);
-
-		if(board1[row][col].equals(" . ")){
-			board1[row][col] = " P ";
-		}else if(board1[row][col].equals(" P ")){
-			board1[row][col] = " . ";
-		}
-	}
-	
-	public boolean select(String[] in){
-	    int row=parse(in[0]);
-	    int col=parse(in[1]);
-	    	    
-	    if(board1[row][col].equals(" P ")){
-	                System.out.println("flag");
-            }else if(board2[row][col].equals(" . ")){
-			board1[row][col] = " - ";
-			//setNumbers();
-            }else if(board2[row][col].equals(" * ")){
-                        System.out.println("You lose °_°");
-                        return false;
+    public void newGame(String[] in){
+        rows=parse(in[0]);
+        cols=parse(in[1]);
+        mins=parse(in[2]);
+        board1 = new String[rows][cols];
+        board2 = new String[rows][cols];
+        board3 = new int[rows][cols];
+        for(int i =0; i < rows;i++){
+            for(int j =0; j < cols;j++){
+                board1[i][j]=" . ";
+                board2[i][j]=" . ";
             }
+        }
+        setMines();
+        calcBoardNumbers();
+        //printBoard();
+        printBoard2();
+        printBoard3();
+    }
+
+    private int parse(String x){
+        return Integer.parseInt(x);
+    }
+    
+    private void setMines(){
+          int cont=0,i,j;
+          while(cont <mins){
+              i=aleatorio.nextInt(rows);
+              j=aleatorio.nextInt(cols);
+              if(board2[i][j].equals(" . ")){
+                  board2[i][j]=" * ";
+                  cont ++;
+              }           
+          }
+    }
+
+    public boolean mark(String[] in){
+        int row=parse(in[0]);
+        int col=parse(in[1]);
+
+        if(board1[row][col].equals(" . ")){
+            board1[row][col] = " P ";
+            contFlags ++;
+            if(board2[row][col].equals(" * ")){
+                contRight ++;
+                return win();//?
+            }          
+        }else if(board1[row][col].equals(" P ")){
+            board1[row][col] = " . ";
+            contFlags --;
+            if(board2[row][col].equals(" * "))contRight --;
+            return win();//?
+        }
+        return true;
+    }
+    
+    public boolean select(String[] in){
+        int row=parse(in[0]);
+        int col=parse(in[1]);
+                
+        if(board1[row][col].equals(" P ")){
+            System.out.println("flag");
+        }else if(board2[row][col].equals(" . ")){
+            board1[row][col] = " - ";
+            freeSpaces(row,col);
+        }else if(board2[row][col].equals(" * ")){
+            showMines();
+            System.out.println("You lose °_°");
+            return false;
+        }
             return true;
-	}
-	
-	public void setBoardNumbers(){
-	    //esquinas
-	    checkCorners();
-	}
-	
-	public void checkCorners(){
-	    if(board2[0][0].equals(" * ")){
-	        board3[0][1]=board3[0][1]+1; board3[1][0]=board3[1][0]+1; board3[1][1]=board3[1][1]+1;
-	    }
-	    if(board2[0][cols-1].equals(" * ")){
-	        board3[0][cols-2]=board3[0][cols-2]+1; board3[1][cols-1]=board3[1][cols-1]+1; board3[1][cols-2]=board3[1][cols-2]+1;
-	    }
-	    if(board2[rows-1][0].equals(" * ")){
-	        board3[rows-2][0]=board3[rows-2][0]+1; board3[rows-2][1]=board3[rows-2][1]+1; board3[rows-1][1]=board3[rows-1][1]+1;
-	    }
-	    if(board2[rows-1][cols-1].equals(" * ")){
-	        board3[rows-2][cols-1]=board3[rows-2][cols-1]+1; board3[rows-2][cols-2]=board3[rows-2][cols-2]+1;
-	        board3[rows-1][cols-2]=board3[rows-1][cols-2]+1;
-	    }
-	}
-	
-	public void checkSides(){
-	    for(int j =1; j < cols-1;j++){
-	        if(board2[0][j].equals(" * ")){
-	            board2[0][j-1]=board2[0][j-1]+1;  
-	        }
-	    }
-	}
-	
-	public void printBoard(){
-	    for(int i =0; i < rows;i++){
-		    for(int j =0; j < cols;j++){
-		        System.out.print(board1[i][j]);
-		    }
-		    System.out.println();
-		}
-	}
-	
-	public void printBoard2(){
-	    for(int i =0; i < rows;i++){
-		    for(int j =0; j < cols;j++){
-		        System.out.print(board2[i][j]);
-		    }
-		    System.out.println();
-		}
-	}
+    }
+    
+    private void calcBoardNumbers(){
+        for(int i =0; i < rows;i++){
+            for(int j =0; j < cols;j++){
+                if(board2[i][j].equals(" * ")){
+                    plus(i-1,j-1); plus(i+1,j);  plus(i-1,j+1);
+                    plus(i,j+1);  /*plus(i,j);*/ plus(i,j-1);
+                    plus(i+1,j-1); plus(i-1,j);  plus(i+1,j+1);
+                }
+            }
+        }
+    }
+    
+    private void plus(int i,int j){
+         if(i>=0 && j>=0 && i<rows && j<cols){
+             board3[i][j]=board3[i][j]+1;
+         }
+    }
+    
+    private void freeSpaces(int i,int j){        
+         /*mine(i-1,j-1);*/ mine(i-1,j); /*mine(i-1,j+1);*/
+         mine(i,j-1);  /*mine(i,j);*/ mine(i,j+1);
+         /*mine(i+1,j-1)*/; mine(i+1,j); /*mine(i+1,j+1);*/        
+    }
+    
+    private void mine(int i,int j){
+         if(i>=0 && j>=0 && i<rows && j<cols){
+             if(board2[i][j].equals(" . ") && board1[i][j].equals(" . ")){
+                 if(board3[i][j]==0){
+                     board1[i][j]=" - ";
+                 }else{
+                     board1[i][j]=" "+board3[i][j]+" ";
+                 }
+                 if(board3[i][j]==0){
+                     freeSpaces(i,j);
+                 }              
+             }
+        }
+    }
+
+    public boolean win(){
+        if(contRight == mins && contFlags == mins){
+                    printBoard();
+                    System.out.println("You won °u°");
+                    return false;
+        }
+        return true;
+    }
+    
+    private void showMines(){
+        for(int i =0; i < rows;i++){
+            for(int j =0; j < cols;j++){
+                if(board2[i][j].equals(" * ")){
+                    board1[i][j]=" * ";
+                }
+            }
+        }
+        printBoard();
+    }
+    
+    public void printBoard(){
+        for(int i =0; i < rows;i++){
+            for(int j =0; j < cols;j++){
+                System.out.print(board1[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+    public void printBoard2(){
+        for(int i =0; i < rows;i++){
+            for(int j =0; j < cols;j++){
+                System.out.print(board2[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    
+    public void printBoard3(){
+        for(int i =0; i < rows;i++){
+            for(int j =0; j < cols;j++){
+                System.out.print(board3[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
--- Minesweeper.java
+++ Minesweeper.java
@@ -1,42 +1,33 @@
 import java.util.Scanner;
 
 public class Minesweeper{
-	public static void main (String[] args){	
-		String[] board, in;
-		boolean playig=true;
-		Scanner sc = new Scanner(System.in);
+    public static void main (String[] args){    
+        String[] in;
+        boolean playing=true;
+        Scanner sc = new Scanner(System.in);
+        Board board = new Board();
 
-		//print rules
-		System.out.println("Minesweeper");
-		System.out.println("rules");
+        //print rules
+        System.out.println("Minesweeper");
+        System.out.println("rules");
+        System.out.println("Entre height, width and mines");
+        in = sc.nextLine().split(" ");
+        board.newGame(in);
 
-		System.out.println("Entre height, width and mines");
-		in = sc.nextLine().split(" ");
-		board.newGame(in);
-
-
-		}
-	}
-
+        do{
+            board.printBoard();
+            System.out.println("enter row, column and an action");
+            in = sc.nextLine().split(" ");
+            
+            if(in[0].equals("exit")){
+                    playing=false;
+            }else if(in[2].equals("M")){
+                playing=board.mark(in);
+            }else if(in[2].equals("U")){
+                playing=board.select(in);
+            }else{
+                System.out.println("command no found");
+            }
+        }while(playing);
+    }
 }
-/*
-print (ingresa alto, ancho y # minas)
-in = leerString.splir(" ")
-board.newGame(in)
-
-	do{
-	borad.printBoard()
-	print(ingrese row, column & action)
-	in = leerString.split(" ")
-	if(in[2].equals("M")){
-		board.Mark(in)
-	}else if(in[2].equals("U")){
-		playing=board.select(in)
-	}else if(in[0]==exit){	
-
-	}else{
-		print(error)
-	} 
-	}while(in[0] != "exit" & playing)
-}
-*/
